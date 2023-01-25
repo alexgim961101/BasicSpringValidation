b0c3eade01d1dcffb840bf023368b4b25e3c6da6
alexgim <alexgim@naver.com>
2023-01-25 17:40:06 +0900

    Valid, Binding reuslt 공부

---
+++ src/main/java/com/alexgim/validation/dto/User.java
@@ -0,0 +1,27 @@
+package com.alexgim.validation.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+
+@Getter @Setter
+@ToString
+public class User {
+
+    @NotBlank
+    private String name;
+
+    @Min(value = 90)
+    private int age;
+
+    @Email
+    private String email;
+
+    @Pattern(regexp = "^\\d{2,3}-\\d{3,4}-\\d{4}$", message = "핸드폰 번호의 양식과 맞지 않습니다.")
+    private String phoneNumber;
+}
